fa7057d1015a1d30bccadf640d074f9e15918497
XCR-1793 <138337647+XCR1793@users.noreply.github.com>
2025-02-27 00:55:22 +1100

    Modified testing files
    Testing nav menu positioning

--- testing/React Testing/React Testing/src/components/header.tsx
+++ testing/React Testing/React Testing/src/components/header.tsx
@@ -1,14 +1,57 @@
+// import Logo from '../assets/Logo.svg'
+// import styles from './header.module.css'
+
+// function Header(){
+//     return(
+//         <>
+//             <header className={styles.header}>
+//                 <nav className={styles.nav}>
+//                     <ul>
+//                         <li>
+//                             <a href="#Store">Store</a>
+//                         </li>
+//                         <li>
+//                             <a href="#Contacts">Contacts</a>
+//                         </li>
+//                         <li>
+//                             <a href="#TOS">TOS</a>
+//                         </li>
+//                     </ul>
+//                 </nav>
+//                 <a target="_blank"><img src={Logo} className={styles.logo}/></a>
+//                 <a href="#FeatherForge" className={styles.featherforge}>Feather Forge</a>
+//             </header>
+//         </>
+//     )
+// }
+
+// export default Header
+
 import Logo from '../assets/Logo.svg'
 import styles from './header.module.css'
 
 function Header(){
     return(
         <>
-            <temp>
-                <a target="_blank">
-                    <img src={Logo} className={styles.logo}/>
+            <header className={styles.header}>
+                <nav className={styles.nav}>
+                    <ul>
+                        <li>
+                            <a href="#Store">Store</a>
+                        </li>
+                        <li>
+                            <a href="#Contacts">Contacts</a>
+                        </li>
+                        <li>
+                            <a href="#TOS">TOS</a>
+                        </li>
+                    </ul>
+                </nav>
+                <a target="_blank" className={styles.logoLink}>
+                    <img src={Logo} className={styles.logo} alt="Logo"/>
                 </a>
-            </temp>
+                <a href="#FeatherForge" className={styles.featherforge}>Feather Forge</a>
+            </header>
         </>
     )
 }
