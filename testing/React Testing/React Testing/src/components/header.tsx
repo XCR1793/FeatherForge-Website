e3033b9e3a377daf93a62b141bd7d6675c000ca9
XCR-1793 <138337647+XCR1793@users.noreply.github.com>
2025-03-01 01:46:53 +1100

    Added Navbar Animation

--- testing/React Testing/React Testing/src/components/header.tsx
+++ testing/React Testing/React Testing/src/components/header.tsx
@@ -3,33 +3,50 @@ import Logo from '../assets/Logo.svg';
 import styles from './header.module.css';
 
 function Header() {
-    return (
-        <header className={styles.header}>
-            <div className={styles.logoContainer}>
-                <Link to="/" className={styles.logoLink}>
-                    <img src={Logo} className={styles.logo} alt="Logo"/>
+  const navItems = [
+    { name: 'Store', links: [{ name: 'Products', to: '/products' }, { name: 'Commissions', to: '/commissions' }] },
+    { name: 'Contacts', to: '/contacts' },
+    { name: 'TOS', to: '/tos' },
+  ];
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoContainer}>
+        <Link to="/" className={styles.logoLink}>
+          <img src={Logo} className={styles.logo} alt="Logo"/>
+        </Link>
+        <Link to="/" className={styles.featherforge} style={{ textDecoration: 'none', color: 'inherit' }}>Feather Forge</Link>
+      </div>
+      <nav className={styles.nav}>
+        <ul>
+          {navItems.map((item, index) => (
+            <li
+              key={index}
+              style={{ animationDelay: `${(index + 1) * 0.2}s` }} // Apply dynamic animation delay
+              className={styles.navItem}
+            >
+              {item.links ? (
+                <>
+                  <span>{item.name}</span>
+                  <div className={styles.dropdownContent}>
+                    {item.links.map((link, subIndex) => (
+                      <Link key={subIndex} to={link.to} style={{ textDecoration: 'none', color: 'inherit' }}>
+                        {link.name}
+                      </Link>
+                    ))}
+                  </div>
+                </>
+              ) : (
+                <Link to={item.to} style={{ textDecoration: 'none', color: 'inherit' }}>
+                  {item.name}
                 </Link>
-                <Link to="/" className={styles.featherforge} style={{ textDecoration: 'none', color: 'inherit' }}>Feather Forge</Link>
-            </div>
-            <nav className={styles.nav}>
-                <ul>
-                    <li className={styles.dropdown}>
-                        <span style={{ textDecoration: 'none', color: 'inherit' }}>Store</span>
-                        <div className={styles.dropdownContent}>
-                            <Link to="/products" style={{ textDecoration: 'none', color: 'inherit' }}>Products</Link>
-                            <Link to="/commissions" style={{ textDecoration: 'none', color: 'inherit' }}>Commissions</Link>
-                        </div>
-                    </li>
-                    <li>
-                        <Link to="/contacts" style={{ textDecoration: 'none', color: 'inherit' }}>Contacts</Link>
-                    </li>
-                    <li>
-                        <Link to="/tos" style={{ textDecoration: 'none', color: 'inherit' }}>TOS</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+              )}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
