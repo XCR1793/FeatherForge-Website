44565aeafecfd58bc869c49636af68f71db2c018
XCR-1793 <138337647+XCR1793@users.noreply.github.com>
2025-03-06 09:22:28 +1100

    Added Home Page Example Image & Modified Animations

--- testing/React Testing/React Testing/src/pages/error404.tsx
+++ testing/React Testing/React Testing/src/pages/error404.tsx
@@ -5,13 +5,13 @@ import styles from './error404.module.css';
 const Error404: React.FC = () => {
     return (
         <>
-            <Header /> {/* Include the Header */}
             <div className={styles.errorContainer}>
                 <div className={styles.errorMessage}>
                     <h1 className={styles.errorTitle}>404 - Page Not Found</h1>
                     <p className={styles.errorDescription}>Sorry, the page you're looking for doesn't exist.</p>
                 </div>
             </div>
+            <Header />
         </>
     );
 }
--- testing/React Testing/React Testing/src/pages/home.tsx
+++ testing/React Testing/React Testing/src/pages/home.tsx
@@ -1,7 +1,7 @@
 import Styles from './home.module.css';
 import Header from '../components/header';
 
-const imageUrl = new URL('../assets/Image.JPG', import.meta.url).toString(); // Resolving image path
+const imageUrl = new URL('../assets/Home Image.png', import.meta.url).toString(); // Resolving image path
 
 function Home() {
     return (
@@ -9,11 +9,16 @@ function Home() {
             <div className={Styles.container}>
                 <img 
                     src={imageUrl} 
-                    alt="Home Background" 
                     className={Styles.image} 
+                    alt="Background" 
                 />
-                <Header /> {/* Header will now be below the image */}
-                <h1 className={Styles.title}>Home</h1>
+                <div className={Styles.projecttext}>
+                    <div className={Styles.projecttitle}>Synth Head</div>
+                    <div className={Styles.projectdesc}>
+                        To create an open source suit which can be worn by an individual and customised to their will. It also must be constructed in such a way that an average human could build it within a reasonable budget and time. The suit must be modular whilst remaining safe for the user to operate. The software must be easy to configure for any individual and has capability to modify without reflashing its firmware. This is a compilation of the best features of ARCOS
+                    </div>
+                </div>
+                <Header/>
             </div>
         </>
     );
