cff9118d67fa275707b283e6a9789a894fda9b44
XCR-1793 <138337647+XCR1793@users.noreply.github.com>
2025-03-06 11:27:38 +1100

    Modified Contacts

--- testing/React Testing/React Testing/src/components/contactTags.tsx
+++ testing/React Testing/React Testing/src/components/contactTags.tsx
@@ -1,20 +1,56 @@
-import styles from 'contactTags.module.css'
+import React from "react";
+import styles from "./contactTags.module.css";
 
-type Contact = {
-    SiteName: string;
-    SiteLogo: string;
-    SiteLink: string;
-    UserName: string;
-    ProfilePic: string;
+// Define TypeScript interface for contact data
+interface Contact {
+  SiteName: string;
+  SiteLogo: string;
+  SiteLink: string;
+  UserName: string;
+  ProfilePic: string;
+}
+
+// Sample JSON data
+const contactData: Contact[] = [
+  {
+    SiteName: "Github",
+    SiteLogo: "../assets/GithubLogo.svg",
+    SiteLink: "https://github.com",
+    UserName: "Userlogo",
+    ProfilePic: "../assets/GithubProfile.png"
+  },
+  {
+    SiteName: "Twitter",
+    SiteLogo: "../assets/TwitterLogo.svg",
+    SiteLink: "https://twitter.com",
+    UserName: "Userlogo",
+    ProfilePic: "../assets/TwitterProfile.png"
+  },
+  {
+    SiteName: "Discord",
+    SiteLogo: "../assets/DiscordLogo.svg",
+    SiteLink: "https://discord.com",
+    UserName: "Userlogo",
+    ProfilePic: "../assets/DiscordProfile.png"
+  }
+];
+
+const ContactTags: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      {contactData.map((contact, index) => (
+        <div key={index} className={styles.card}>
+          <img src={contact.ProfilePic} alt={`${contact.UserName} profile`} className={styles.profilePic} />
+          <div className={styles.info}>
+            <img src={contact.SiteLogo} alt={`${contact.SiteName} logo`} className={styles.siteLogo} />
+            <a href={contact.SiteLink} target="_blank" rel="noopener noreferrer">
+              {contact.SiteName}
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-function contactTag(){
-    
-}
-
-function contactTags(){
-    return (
-      );
-}
-
-export default contactTags;
+export default ContactTags;
--- testing/React Testing/React Testing/src/pages/contacts.tsx
+++ testing/React Testing/React Testing/src/pages/contacts.tsx
@@ -1,15 +1,15 @@
 import Header from '../components/header';
-// import ContactTags from '../components/contactTags';
+import ContactTags from '../components/contactTags';
 
 function Contacts() {
     return (
         <>
             <Header />
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw' }}>
+            {/* <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw' }}>
                 <h1 style={{ fontSize: '3rem', fontWeight: 'bold', textAlign: 'center' }}>Contacts</h1>
-            </div>
+            </div> */}
 
-            {/* <ContactTags /> */}
+            <ContactTags />
 
         </>
     );
